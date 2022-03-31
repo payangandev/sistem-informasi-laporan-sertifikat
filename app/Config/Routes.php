bb9ba6aedd999b3d6a6a46e7dba0bf01cb503411
Deni Gunawan <denigunawan775@gmail.com>
2022-03-31 19:36:22 +0700

    done

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -55,67 +55,58 @@ $routes->post('karyawan/update/(:num)', 'KaryawanController::update/$1');
 $routes->get('karyawan/delete/(:alphanum)', 'KaryawanController::delete/$1');
 
 
-// Routing for supplier
-$routes->get('supplier/pdf', 'SupplierController::pdf');
-$routes->get('supplier/excel', 'SupplierController::excel');
-$routes->get('supplier', 'SupplierController::index');
-$routes->get('supplier/create', 'SupplierController::create');
-$routes->post('supplier/store', 'SupplierController::store');
-$routes->get('supplier/edit/(:alphanum)', 'SupplierController::edit/$1');
-$routes->post('supplier/update/(:num)', 'SupplierController::update/$1');
-$routes->get('supplier/delete/(:alphanum)', 'SupplierController::delete/$1');
+// Routing for Iso
+$routes->get('iso/pdf', 'IsoController::pdf');
+$routes->get('iso/excel', 'IsoController::excel');
+$routes->get('iso', 'IsoController::index');
+$routes->get('iso/create', 'IsoController::create');
+$routes->post('iso/store', 'IsoController::store');
+$routes->get('iso/edit/(:alphanum)', 'IsoController::edit/$1');
+$routes->post('iso/update/(:num)', 'IsoController::update/$1');
+$routes->get('iso/delete/(:alphanum)', 'IsoController::delete/$1');
 
 
-// Routing for  celana
-$routes->get('celana/pdf', 'CelanaController::pdf');
-$routes->get('celana/excel', 'CelanaController::excel');
-$routes->get('celana', 'CelanaController::index');
-$routes->get('celana/create', 'CelanaController::create');
-$routes->post('celana/store', 'CelanaController::store');
-$routes->get('celana/edit/(:alphanum)', 'CelanaController::edit/$1');
-$routes->post('celana/update/(:num)', 'CelanaController::update/$1');
-$routes->get('celana/delete/(:alphanum)', 'CelanaController::delete/$1');
+// Routing for  Ska
+$routes->get('ska/pdf', 'SkaController::pdf');
+$routes->get('ska/excel', 'SkaController::excel');
+$routes->get('ska', 'SkaController::index');
+$routes->get('ska/create', 'SkaController::create');
+$routes->post('ska/store', 'SkaController::store');
+$routes->get('ska/edit/(:alphanum)', 'SkaController::edit/$1');
+$routes->post('ska/update/(:num)', 'SkaController::update/$1');
+$routes->get('ska/delete/(:alphanum)', 'SkaController::delete/$1');
 
 
-// Routing for  kemeja
-$routes->get('kemeja/pdf', 'KemejaController::pdf');
-$routes->get('kemeja/excel', 'KemejaController::excel');
-$routes->get('kemeja', 'KemejaController::index');
-$routes->get('kemeja/create', 'KemejaController::create');
-$routes->post('kemeja/store', 'KemejaController::store');
-$routes->get('kemeja/edit/(:alphanum)', 'KemejaController::edit/$1');
-$routes->post('kemeja/update/(:num)', 'KemejaController::update/$1');
-$routes->get('kemeja/delete/(:alphanum)', 'KemejaController::delete/$1');
+// Routing for  Skt
+$routes->get('skt/pdf', 'SktController::pdf');
+$routes->get('skt/excel', 'SktController::excel');
+$routes->get('skt', 'SktController::index');
+$routes->get('skt/create', 'SktController::create');
+$routes->post('skt/store', 'SktController::store');
+$routes->get('skt/edit/(:alphanum)', 'SktController::edit/$1');
+$routes->post('skt/update/(:num)', 'SktController::update/$1');
+$routes->get('skt/delete/(:alphanum)', 'SktController::delete/$1');
 
-// Routing for jaket
-$routes->get('jaket/pdf', 'JaketController::pdf');
-$routes->get('jaket/excel', 'JaketController::excel');
-$routes->get('jaket', 'JaketController::index');
-$routes->get('jaket/create', 'JaketController::create');
-$routes->post('jaket/store', 'JaketController::store');
-$routes->get('jaket/edit/(:alphanum)', 'JaketController::edit/$1');
-$routes->post('jaket/update/(:num)', 'JaketController::update/$1');
-$routes->get('jaket/delete/(:alphanum)', 'JaketController::delete/$1');
+// Routing for Perusahaan
+$routes->get('perusahaan/pdf', 'PerusahaanController::pdf');
+$routes->get('perusahaan/excel', 'PerusahaanController::excel');
+$routes->get('perusahaan', 'PerusahaanController::index');
+$routes->get('perusahaan/create', 'PerusahaanController::create');
+$routes->post('perusahaan/store', 'PerusahaanController::store');
+$routes->get('perusahaan/edit/(:alphanum)', 'PerusahaanController::edit/$1');
+$routes->post('perusahaan/update/(:num)', 'PerusahaanController::update/$1');
+$routes->get('perusahaan/delete/(:alphanum)', 'PerusahaanController::delete/$1');
 
-// Routing for tshirt
-$routes->get('tshirt/pdf', 'TShirtController::pdf');
-$routes->get('tshirt/excel', 'TShirtController::excel');
-$routes->get('tshirt', 'TShirtController::index');
-$routes->get('tshirt/create', 'TShirtController::create');
-$routes->post('tshirt/store', 'TShirtController::store');
-$routes->get('tshirt/edit/(:alphanum)', 'TShirtController::edit/$1');
-$routes->post('tshirt/update/(:num)', 'TShirtController::update/$1');
-$routes->get('tshirt/delete/(:alphanum)', 'TShirtController::delete/$1');
+// Routing for Ktiga
+$routes->get('ktiga/pdf', 'KtigaController::pdf');
+$routes->get('ktiga/excel', 'KtigaController::excel');
+$routes->get('ktiga', 'KtigaController::index');
+$routes->get('ktiga/create', 'KtigaController::create');
+$routes->post('ktiga/store', 'KtigaController::store');
+$routes->get('ktiga/edit/(:alphanum)', 'KtigaController::edit/$1');
+$routes->post('ktiga/update/(:num)', 'KtigaController::update/$1');
+$routes->get('ktiga/delete/(:alphanum)', 'KtigaController::delete/$1');
 
-// Routing for belt
-$routes->get('belt/pdf', 'BeltController::pdf');
-$routes->get('belt/excel', 'BeltController::excel');
-$routes->get('belt', 'BeltController::index');
-$routes->get('belt/create', 'BeltController::create');
-$routes->post('belt/store', 'BeltController::store');
-$routes->get('belt/edit/(:alphanum)', 'BeltController::edit/$1');
-$routes->post('belt/update/(:num)', 'BeltController::update/$1');
-$routes->get('belt/delete/(:alphanum)', 'BeltController::delete/$1');
 
 
 
